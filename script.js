257b91630b8919d6e297366f76d2f054669de702
FKNoyan <egemennoyan@hotmail.com>
2021-10-29 12:28:10 +0300

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let display = document.getElementById("display");
 let buttons = Array.from(document.getElementsByClassName("button"));
 /* Bir tane bug var. İşlem sonucu ekrana gelen sayının sonuna, başka sayılar eklenebiliyor. Bunu engellemek lazım */
-//Basılan sembol butonları eşittir işareti gelene kadar tekrar basılır hale GELMESİN!
 //Ekrandaki yazının, yeni sayı girilirken değişmesi, yeni sayıya eşit olması gerek.
 const equalButton = document.getElementById("equal-btn");
 const plusButton = document.getElementById("plus-btn");
@@ -23,86 +22,103 @@ function enableButton() {
   divisionButton.disabled = false;
 }
 
+/*display ekranında bir işaret olup olmadığını
+ (String(checkArray[i]) === "NaN") ayarında bir şey kullanarak yapabilirim. Böylelikle del tuşuna basıldığında sembol butonları
+ dinamik olarak disable/enable olabilir*/
+
+//This function calculates the inner text of a given string value
+function calculate(str) {
+  let strArray = Array.from(str);
+  let checkArray = [];
+  for (let i = 0; i < strArray.length; i++) {
+    checkArray.push(Number(strArray[i]));
+    if (String(checkArray[i]) === "NaN") {
+      //The math symbol in the str parameter is revealed
+      const arSymbol = strArray[i];
+      //Numbers from strArray
+      let num1 = Number(strArray.slice(0, i).join().replaceAll(",", ""));
+      let num2 = Number(
+        strArray
+          .slice(i + 1)
+          .join()
+          .replaceAll(",", "")
+      );
+      switch (arSymbol) {
+        case "*":
+          str = String(num1 * num2);
+          break;
+        case "+":
+          str = String(num1 + num2);
+          break;
+        case "/":
+          str = String(num1 / num2);
+          break;
+        case "-":
+          str = String(num1 - num2);
+          break;
+        default:
+          console.log("DEFAULT");
+          break;
+      }
+    }
+  }
+  return str;
+}
+
+//Disable button yapmakta geç kalıyor. Nan'ı bir sonraki button clickte görüyor
+function checkNan(str) {
+  let strArray = Array.from(str);
+  let checkArray = [];
+  let stringArray = [];
+
+  for (let i = 0; i < strArray.length; i++) {
+    checkArray.push(Number(strArray[i]));
+    stringArray.push(String(checkArray[i]));
+  }
+  if (stringArray.includes("NaN")) {
+    disableButton();
+  } else enableButton();
+}
+
+//EVENTS BEGIN
+
 buttons.map((button) => {
   button.addEventListener("click", (e) => {
+    //this nested if statement checks whether a button's innerhtml equals to NaN, other than RESET and DEl. That way, I am able to distinguish mathematical symbols and disable their buttons, for convenience.
+    if (
+      e.target.innerText !== "RESET" &&
+      e.target.innerText !== "DEL" &&
+      e.target.innerText !== "."
+    ) {
+      if (String(Number(e.target.innerText)) === "NaN") {
+        disableButton();
+        console.log(String(Number(e.target.innerText)));
+      }
+    }
     switch (e.target.innerText) {
-      case "+":
-        display.innerText += e.target.innerText;
-        disableButton();
-        break;
-      // case "*":
-      //   // display.innerText += e.target.innerText;
-      //   // disableButton();
-      //   break;
-      case "/":
-        display.innerText += e.target.innerText;
-        disableButton();
-        break;
-      case "-":
-        display.innerText += e.target.innerText;
-        disableButton();
-        break;
-
       case "RESET":
         display.innerText = "";
         enableButton();
         break;
       case "DEL":
-        // let delArray = Array.from(display.innerText);
-        // for (let i = 0; i < delArray.length; i++) {
-        //   let currentElement = delArray[i];
-        //   parseInt(currentElement);
-        //   if (isNaN(currentElement)) {
-        //     enableButton();
-        //   }
-        // }
-
+        //Del works fine. If a math symbol is deleted, their buttons get reactivated.
+        let displayArray = [];
+        displayArray = Array.from(display.innerText);
+        let lastItem = displayArray.pop();
+        console.log(String(Number(lastItem)));
+        if (String(Number(lastItem)) === "NaN") {
+          enableButton();
+          console.log(lastItem);
+        }
         display.innerText = display.innerText.slice(
           0,
           display.innerText.length - 1
         );
-        let lastItem = display.innerText.slice(display.innerText.length - 1);
-        if (lastItem == "+") {
-          enableButton();
-          console.log(lastItem);
-        }
 
         break;
       case "=":
-        // Bu calculate arrayini sembol ayıklamak için yazdım
-        let calculateArray = Array.from(display.innerText);
-        let mathSymbol = "+";
-        for (let i = 0; i < calculateArray.length; i++) {
-          mathSymbol = calculateArray[i];
-          if (mathSymbol == "+") {
-            let numsArray = display.innerText.split("+");
-            let num1 = parseFloat(numsArray[0]);
-            let num2 = parseFloat(numsArray[1]);
-            display.innerText = num1 + num2;
-            console.log(num1 + num2);
-          } else if (mathSymbol == "*") {
-            console.log("Sembol +");
-            let numsArray = display.innerText.split("*");
-            let num1 = parseFloat(numsArray[0]);
-            let num2 = parseFloat(numsArray[1]);
-            display.innerText = num1 * num2;
-            console.log(num1 * num2);
-          } else if (mathSymbol == "-") {
-            console.log("Sembol +");
-            let numsArray = display.innerText.split("-");
-            let num1 = parseFloat(numsArray[0]);
-            let num2 = parseFloat(numsArray[1]);
-            display.innerText = num1 - num2;
-            console.log(num1 - num2);
-          } else if (mathSymbol == "/") {
-            console.log("Sembol +");
-            let numsArray = display.innerText.split("/");
-            let num1 = parseFloat(numsArray[0]);
-            let num2 = parseFloat(numsArray[1]);
-            display.innerText = num1 / num2;
-            console.log(num1 / num2);
-          }
-        }
+        //Bu eşleştirmeye bayılıyorum
+        display.innerText = calculate(display.innerText);
         enableButton();
         break;
       default:
